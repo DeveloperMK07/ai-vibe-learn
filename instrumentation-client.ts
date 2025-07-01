73e239fbbb26c6b84a1a873f9bcccf2f0a4f5e32
unknown <mayur7024k@gmail.com>
2025-07-01 20:37:53 +0530

    Conversation and session history implementation

---
+++ instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e3c6ddf3d731244d718cf7c803d17f38@o4509401238470656.ingest.us.sentry.io/4509588279001088",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
