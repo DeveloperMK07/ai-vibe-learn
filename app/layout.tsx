073107cdc0e31a230979dc20907fb59487754e3e
unknown <mayur7024k@gmail.com>
2025-07-10 23:45:12 +0530

    final change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const bricolage = Bricolage_Grotesque({
 });
 
 export const metadata: Metadata = {
-  title: "Converso",
+  title: "Vibe Learn-AI",
   description: "Real-time AI Teaching Platform",
 };
 
